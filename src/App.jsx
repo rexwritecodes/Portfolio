5678541b32dbc73c444667ec825b0dfdd48874d3
rexwritecodes <pranavpanchwatkar@outloo.com>
2025-05-24 09:26:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-red-600">Tailwind is working!</h1>
+    </div>
+  );
+}
+
+export default App;
